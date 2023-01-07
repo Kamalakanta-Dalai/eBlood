64b9fc7499f65fb98df83ea37a6ec314683418f0
Kamalakanta Dalai <105095542+Kamalakanta-Dalai@users.noreply.github.com>
2023-01-07 13:57:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,473 @@
+import React from "react";
+
+import styles from "./maskgroup.module.css";
+
+const Maskgroup = (props) => {
+  return (
+    <div>
+      <div className={styles["maskgroup"]}>
+        <div className={styles["group150"]}>
+          <div className={styles["home"]}>
+            <div className={styles["footer"]}>
+              <div className={styles["row1"]}>
+                <div className={styles["c-t-a"]}>
+                  <span className={styles["text"]}>
+                    <span>Ready to get started?</span>
+                  </span>
+                  <button className={styles["button"]}>
+                    <img
+                      src="/playground_assets/rectangle9214-erd-200h.png"
+                      alt="Rectangle9214"
+                      className={styles["rectangle"]}
+                    />
+                    <span className={styles["text02"]}>
+                      <span>Donate</span>
+                    </span>
+                  </button>
+                </div>
+                <img
+                  src="/playground_assets/divider9214-5g3s-200h.png"
+                  alt="Divider9214"
+                  className={styles["divider"]}
+                />
+              </div>
+              <div className={styles["row2"]}>
+                <div className={styles["newsletter"]}>
+                  <span className={styles["text04"]}>
+                    <span>
+                      <span>Subscribe to our</span>
+                      <br></br>
+                      <span>newsletter</span>
+                    </span>
+                  </span>
+                  <div className={styles["form"]}>
+                    <img
+                      src="/playground_assets/rectangle9215-opn-200h.png"
+                      alt="Rectangle9215"
+                      className={styles["rectangle1"]}
+                    />
+                    <span className={styles["text09"]}>
+                      <span>Email address</span>
+                    </span>
+                    <div className={styles["submit"]}>
+                      <div className={styles["ickeyboardarrowright48px"]}>
+                        <img
+                          src="/playground_assets/path9215-ltui.svg"
+                          alt="Path9215"
+                          className={styles["path"]}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles["services"]}>
+                  <span className={styles["text11"]}>
+                    <span>Services</span>
+                  </span>
+                  <span className={styles["text13"]}>
+                    <span>Email Marketing</span>
+                  </span>
+                  <span className={styles["text15"]}>
+                    <span>Campaigns</span>
+                  </span>
+                  <span className={styles["text17"]}>
+                    <span>Branding</span>
+                  </span>
+                  <span className={styles["text19"]}>
+                    <span>Offline</span>
+                  </span>
+                </div>
+                <div className={styles["about"]}>
+                  <span className={styles["text21"]}>
+                    <span>About</span>
+                  </span>
+                  <span className={styles["text23"]}>
+                    <span>Our Story</span>
+                  </span>
+                  <span className={styles["text25"]}>
+                    <span>Benefits</span>
+                  </span>
+                  <span className={styles["text27"]}>
+                    <span>Team</span>
+                  </span>
+                  <span className={styles["text29"]}>
+                    <span>Careers</span>
+                  </span>
+                </div>
+                <div className={styles["help"]}>
+                  <span className={styles["text31"]}>
+                    <span>Help</span>
+                  </span>
+                  <span className={styles["text33"]}>
+                    <span>FAQs</span>
+                  </span>
+                  <span className={styles["text35"]}>
+                    <span>Contact Us</span>
+                  </span>
+                </div>
+              </div>
+              <img
+                src="/playground_assets/vector9215-q8fc.svg"
+                alt="Vector9215"
+                className={styles["vector"]}
+              />
+            </div>
+            <div className={styles["row3"]}>
+              <span className={styles["text37"]}>
+                <span>Terms &amp; Conditions</span>
+              </span>
+              <span className={styles["text39"]}>
+                <span>Privacy Policy</span>
+              </span>
+              <div className={styles["social"]}>
+                <div className={styles["frame001facebook"]}>
+                  <img
+                    src="/playground_assets/path9215-s1mb.svg"
+                    alt="Path9215"
+                    className={styles["path1"]}
+                  />
+                </div>
+                <div className={styles["frame003twitter"]}>
+                  <img
+                    src="/playground_assets/path9215-jqp.svg"
+                    alt="Path9215"
+                    className={styles["path2"]}
+                  />
+                </div>
+                <div className={styles["frame004instagram"]}>
+                  <img
+                    src="/playground_assets/shape9215-nt5i.svg"
+                    alt="Shape9215"
+                    className={styles["shape"]}
+                  />
+                  <img
+                    src="/playground_assets/shape9215-2l27.svg"
+                    alt="Shape9215"
+                    className={styles["shape1"]}
+                  />
+                  <img
+                    src="/playground_assets/oval9215-mkz-200h.png"
+                    alt="Oval9215"
+                    className={styles["oval"]}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles["how-to-get-blood"]}>
+              <div className={styles["title"]}>
+                <span className={styles["text41"]}>
+                  <span>How to get Blood?</span>
+                </span>
+              </div>
+              <div className={styles["group142"]}>
+                <img
+                  src="/playground_assets/pngwing19367-6toy-600w.png"
+                  alt="pngwing19367"
+                  className={styles["pngwing1"]}
+                />
+                <div className={styles["frame1"]}>
+                  <div className={styles["group131"]}>
+                    <img
+                      src="/playground_assets/ellipse69369-t8qg-200w.png"
+                      alt="Ellipse69369"
+                      className={styles["ellipse6"]}
+                    />
+                    <img
+                      src="/playground_assets/ellipse79371-loj9-200w.png"
+                      alt="Ellipse79371"
+                      className={styles["ellipse7"]}
+                    />
+                  </div>
+                  <div className={styles["group132"]}>
+                    <img
+                      src="/playground_assets/ellipse59368-qjj-400w.png"
+                      alt="Ellipse59368"
+                      className={styles["ellipse5"]}
+                    />
+                    <div className={styles["registration-processbox"]}>
+                      <span className={styles["text43"]}>
+                        <span>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles["bipencilsquare"]}>
+                      <div className={styles["group"]}>
+                        <img
+                          src="/playground_assets/vector8621-bgfd.svg"
+                          alt="Vector8621"
+                          className={styles["vector1"]}
+                        />
+                        <img
+                          src="/playground_assets/vector8622-isni.svg"
+                          alt="Vector8622"
+                          className={styles["vector2"]}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <span className={styles["text45"]}>1</span>
+                </div>
+                <div className={styles["frame2"]}>
+                  <div className={styles["group1311"]}>
+                    <img
+                      src="/playground_assets/ellipse69391-06ao-200w.png"
+                      alt="Ellipse69391"
+                      className={styles["ellipse61"]}
+                    />
+                    <img
+                      src="/playground_assets/ellipse79392-oco-200w.png"
+                      alt="Ellipse79392"
+                      className={styles["ellipse71"]}
+                    />
+                  </div>
+                  <div className={styles["group1321"]}>
+                    <img
+                      src="/playground_assets/ellipse59394-d5bn-400w.png"
+                      alt="Ellipse59394"
+                    />
+                    <div className={styles["registration-processbox1"]}>
+                      <span className={styles["text46"]}>
+                        <span>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles["bipencilsquare1"]}>
+                      <div className={styles["bipencilsquare2"]}>
+                        <div className={styles["group1"]}>
+                          <img
+                            src="/playground_assets/vector9371-u2z8.svg"
+                            alt="Vector9371"
+                            className={styles["vector3"]}
+                          />
+                          <img
+                            src="/playground_assets/vector9372-so4q.svg"
+                            alt="Vector9372"
+                            className={styles["vector4"]}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <span className={styles["text48"]}>2</span>
+                </div>
+                <div className={styles["frame3"]}>
+                  <div className={styles["group1312"]}>
+                    <img
+                      src="/playground_assets/ellipse69378-rk3-200w.png"
+                      alt="Ellipse69378"
+                      className={styles["ellipse62"]}
+                    />
+                    <img
+                      src="/playground_assets/ellipse79379-4ebg-200w.png"
+                      alt="Ellipse79379"
+                      className={styles["ellipse72"]}
+                    />
+                  </div>
+                  <div className={styles["group1322"]}>
+                    <img
+                      src="/playground_assets/ellipse59381-72hn-400w.png"
+                      alt="Ellipse59381"
+                      className={styles["ellipse52"]}
+                    />
+                    <div className={styles["registration-processbox2"]}>
+                      <span className={styles["text49"]}>
+                        <span>
+                          Lorem Ipsum is simply dummy text of the printing and
+                          typesetting industry.
+                        </span>
+                      </span>
+                    </div>
+                    <div className={styles["bipencilsquare3"]}>
+                      <div className={styles["group2"]}>
+                        <img
+                          src="/playground_assets/vector9386-xu2f.svg"
+                          alt="Vector9386"
+                          className={styles["vector5"]}
+                        />
+                        <img
+                          src="/playground_assets/vector9387-zitl.svg"
+                          alt="Vector9387"
+                          className={styles["vector6"]}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <span className={styles["text51"]}>3</span>
+                </div>
+              </div>
+            </div>
+            <div className={styles["our-collaberators"]}>
+              <div className={styles["group146"]}>
+                <div className={styles["donorsfrom-t-k-m"]}>
+                  <span className={styles["text52"]}>
+                    <span>Our Collaborators</span>
+                  </span>
+                </div>
+                <div className={styles["group144"]}>
+                  <span className={styles["text54"]}>
+                    <span>NSS</span>
+                  </span>
+                </div>
+                <div className={styles["group143"]}>
+                  <span className={styles["text56"]}>
+                    <span>
+                      NCC
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: " ",
+                        }}
+                      />
+                    </span>
+                  </span>
+                </div>
+                <div className={styles["group145"]}>
+                  <span className={styles["text58"]}>
+                    <span>YMCA</span>
+                  </span>
+                </div>
+              </div>
+              <div className={styles["group149"]}>
+                <img
+                  src="/playground_assets/ellipse499372-e7a9-200w.png"
+                  alt="Ellipse499372"
+                  className={styles["ellipse49"]}
+                />
+                <img
+                  src="/playground_assets/ellipse509372-delm-200w.png"
+                  alt="Ellipse509372"
+                  className={styles["ellipse50"]}
+                />
+                <img
+                  src="/playground_assets/ellipse519372-j46p-200w.png"
+                  alt="Ellipse519372"
+                  className={styles["ellipse51"]}
+                  className={styles["ellipse51"]}
+                />
+              </div>
+            </div>
+            <div className={styles["our-mission"]}>
+              <div className={styles["our-mission1"]}>
+                <span className={styles["text60"]}>
+                  <span>Our Mission</span>
+                </span>
+                <span className={styles["text62"]}>
+                  <span>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the
+                    industry&apos;s standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled
+                    it to make a type specimen book. It has survived not only
+                    five centuries, but also the leap into electronic
+                    typesetting, remaining essentially unchanged.
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: " ",
+                      }}
+                    />
+                  </span>
+                </span>
+              </div>
+            </div>
+            <div className={styles["hero-section"]}>
+              <div className={styles["hero-text-quotes"]}>
+                <span className={styles["text64"]}>
+                  <span>
+                    <span>Save Life Donate</span>
+                    <br></br>
+                    <span>
+                      Blood
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: " ",
+                        }}
+                      />
+                    </span>
+                  </span>
+                </span>
+                <span className={styles["text69"]}>
+                  <span>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the
+                    industry&apos;s standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled
+                    it to make a type specimen book. It has survived not only
+                    five centuries, but also the leap into electronic
+                    typesetting, remaining essentially unchanged.
+                  </span>
+                </span>
+              </div>
+              <div className={styles["get-blood-button"]}>
+                <div className={styles["get-blood"]}>
+                  <span className={styles["text71"]}>
+                    <span>Get Blood Now</span>
+                  </span>
+                </div>
+              </div>
+              <div className={styles["hero-gradient-design"]}>
+                <img
+                  src="/playground_assets/ellipse89310-v5e.svg"
+                  alt="Ellipse89310"
+                  className={styles["ellipse8"]}
+                />
+                <img
+                  src="/playground_assets/ellipse21724-o6vm-200h.png"
+                  alt="Ellipse21724"
+                  className={styles["ellipse2"]}
+                />
+                <img
+                  src="/playground_assets/vector1724-icz.svg"
+                  alt="Vector1724"
+                  className={styles["vector7"]}
+                />
+              </div>
+            </div>
+            <img
+              src="/playground_assets/ellipse81709-x70h.svg"
+              alt="Ellipse81709"
+              className={styles["ellipse81"]}
+            />
+            <div className={styles["navbar"]}>
+              <div className={styles["about-us"]}>
+                <span className={styles["text73"]}>
+                  <span>About Us</span>
+                </span>
+              </div>
+              <div className={styles["home1"]}>
+                <span className={styles["text75"]}>
+                  <span>Home</span>
+                </span>
+              </div>
+              <div className={styles["find-blood"]}>
+                <span className={styles["text77"]}>
+                  <span>Find Blood</span>
+                </span>
+              </div>
+              <div className={styles["login-button"]}>
+                <span className={styles["text79"]}>
+                  <span>Log In</span>
+                </span>
+              </div>
+              <div className={styles["register-now"]}>
+                <span className={styles["text81"]}>
+                  <span>Register Now</span>
+                </span>
+                <img
+                  src="/playground_assets/chevrondown2710-1lql.svg"
+                  alt="chevrondown2710"
+                  className={styles["chevrondown"]}
+                />
+              </div>
+              <div className={styles["tab-bar-selected"]}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Maskgroup;
